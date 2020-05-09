de12fb7e893de071c52798e562b18573ec8d85c2
tempname1533 <pedro_de_sousa@outlook.com>
2020-05-09 12:10:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useDarkMode from "./use-dark-mode";
+import Toggle from "./Toggle";
+import Content from "./Content";
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <div> Home</div>
+        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+      </div>
+      <Content />
     </div>
   );
 }
